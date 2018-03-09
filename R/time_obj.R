5fa31dce59df2d3a3943800ddf86ff841a11c522
Nathan Wycoff <nathanbrwycoff@gmail.com>
2018-03-09 13:23:11 -0500

    Added year tests

--- R/time_obj.R
+++ R/time_obj.R
@@ -20,10 +20,30 @@ toplot <- function(target, desc) {
     print(p)
 }
 
+#Do an ANOVA to see if any year was sig diff from others
+test <- function(target) {
+    summary(lm(target ~ I(as.factor(df$Year))))
+}
+
+#Do the tests!
+run_test(name = 'count_by_year', test = test, toplot = toplot)
+
+## See if frequency changes over time.
+toplot <- function(target, desc) {
+    plotdf <- data.frame(target = as.numeric(target > 0), Year = df$Year)
+    aggdf <- aggregate(plotdf$target, by = list(plotdf$Year), mean)
+    colnames(aggdf) <- c('Year', 'target')
+    p <- ggplot(aggdf, aes(y = target, x = Year)) + 
+        geom_line() + ggtitle(paste(desc, "by Year")) +
+        xlab('Genre') + ylab('Objectification Frequency') + ylim(0, 1)
+
+    print(p)
+}
+
 #For now, no test
 test <- function(target) {
     'nothing yet'
 }
 
 #Do the tests!
-run_test(name = 'count_by_year', test = test, toplot = toplot)
+run_test(name = 'freq_by_year', test = test, toplot = toplot)
